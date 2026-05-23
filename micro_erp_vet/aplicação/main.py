79bbc874d6460d90b4ea9cd644f028db42a8da3c
ftrindadj <ftjustino21@gmail.com>
2026-05-23 16:35:50 -0300

    feat: atualizando o main.py

--- micro_erp_vet/aplicação/main.py
+++ micro_erp_vet/aplicação/main.py
@@ -9,7 +9,17 @@ app = FastAPI(title="Micro-ERP Veterinária")
 
 # Inclui as rotas do sistema
 app.include_router(cadastros.router)
+app.include_router(compras.router)
+app.include_router(vendas.router)
+app.include_router(financeiro.router)
+app.include_router(contabilidade.router)
+
+
 
 @app.get("/")
 def home():
-    return {"status": "Sistema Online", "sprint": 1}
+    return {
+        "status": "Sistema Online", 
+        "projeto": "Micro-ERP Veterinária",
+        "versao": "Final (Sprints 1-5)"  
+    }
